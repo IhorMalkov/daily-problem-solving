c8e7a00645a5852c287cbbfdbee4d4494b398ad7
Ihor Malkov <igorzno2004@gmail.com>
2026-09-03 23:00:43 -0700

    Seconda taak

---
+++ leetcode/01-hashtable/002-contains-duplicate.js
@@ -0,0 +1,14 @@
+containsDuplicate = function (nums) {
+  let set = new Set();
+
+  for (let num of nums) {
+    if (set.has(num)) {
+      return true;
+    }
+    set.add(num);
+  }
+
+  return false;
+};
+
+console.log(containsDuplicate([1, 2, 3]));
