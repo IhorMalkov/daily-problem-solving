5b79d1cea90a88ad7a169d0cbcf17d78b811b534
Ihor Malkov <igorzno2004@gmail.com>
2026-09-19 21:41:13 -0700

    DailyTemperature:

---
+++ leetcode/03-stack/018-DailyTemperature.js
@@ -0,0 +1,20 @@
+var dailyTemperatures = function (temperatures) {
+  let stack = [];
+  const result = new Array(temperatures.length).fill(0);
+
+  for (let i = 0; i < temperatures.length; i++) {
+    while (
+      stack.length > 0 &&
+      temperatures[i] > temperatures[stack[stack.length - 1]]
+    ) {
+      const idx = stack.pop();
+      result[idx] = i - idx;
+    }
+    stack.push(i);
+  }
+  return result;
+};
+
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])); // [1, 1, 4, 2, 1, 1, 0, 0]
+console.log(dailyTemperatures([30, 40, 50, 60])); // [1, 1, 1, 0]
+console.log(dailyTemperatures([30, 60, 90])); // [1, 1, 0]
